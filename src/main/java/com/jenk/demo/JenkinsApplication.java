ae9eab6f819920ab633edab9f4a5ada4a6426f92
Ashu-029 <ashishsawale2902@gmail.com>
2023-10-28 12:50:13 +0530

    added new file

--- src/main/java/com/jenk/demo/JenkinsApplication.java
+++ src/main/java/com/jenk/demo/JenkinsApplication.java
@@ -1,14 +1,27 @@
 package com.jenk.demo;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsApplication {
 
+	
+	private static final Logger logger = LoggerFactory.getLogger(JenkinsApplication.class);
+
+	@PostConstruct
+	public void init() {
+		logger.info("Application Started.... ");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
-	System.out.println(1021);
+	    logger.info("Application Executed... ");
+		System.out.println("Ashish");
 	}
 
 }
