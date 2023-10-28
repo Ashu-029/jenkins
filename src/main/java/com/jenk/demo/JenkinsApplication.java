a9c14190700aef46d458efcd295115ddf3f3359b
Ashu-029 <ashishsawale2902@gmail.com>
2023-10-28 15:40:11 +0530

    Changes made successfully

--- src/main/java/com/jenk/demo/JenkinsApplication.java
+++ src/main/java/com/jenk/demo/JenkinsApplication.java
@@ -15,7 +15,7 @@ public class JenkinsApplication {
 
 	@PostConstruct
 	public void init() {
-		logger.info("Application Started.. ... ");
+		logger.info("Application Started Again ... ");
 	}
 	
 	public static void main(String[] args) {
